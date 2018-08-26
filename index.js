876cdf1a7ded7ab70c597bef0b666992f222845b
artset <katsang18@gmail.com>
2018-08-25 21:23:53 -0500

    new layout
    the same look u love just actually functional for mobile phones, also
    NEW PAGES?? extra features? lots of fun stuff inspired by maurice ravel,
    powered (and sponsored?) by all the adobe products i needed to make this
    work.

--- index.js
+++ index.js
@@ -1,53 +1,29 @@
-$(document).ready(function(){$("body").hide().fadeIn(1000);});
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-document.getElementById("portfolio_link").addEventListener("click", showPort);
-document.getElementById("pclose").addEventListener("click", closePort);
+// Get the navbar
+var navbar = document.getElementById("create");
 
-document.getElementById("about_link").addEventListener("click", showAbout);
-document.getElementById("cclose").addEventListener("click", closePort);
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-function showAbout() {
-    var aboutDiv = document.getElementById("about");
-    var sideDiv = document.getElementById("sidebar");
-    var portDiv = document.getElementById("portfolio");
-    var aboutOp = aboutDiv.style.opacity;
-    if (aboutOp == 0) {
-        portDiv.style.opacity = 0;
-        aboutDiv.style.opacity = 1;
-        sideDiv.style.marginLeft = "-350px";
-        sideDiv.style.opacity = .4;
-    } else {
-        aboutDiv.style.opacity = 0;
-        sideDiv.style.marginLeft = "-170px";
-        sideDiv.style.opacity = 1;
-    }
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
 
+var curates = document.getElementById("curate");
 
-function showPort() {
-    var portDiv = document.getElementById("portfolio");
-    var sideDiv = document.getElementById("sidebar");
-    var aboutDiv = document.getElementById("about");
-    var portOp = portDiv.style.opacity;
-    
-    if (portOp == 0) {
-        aboutDiv.style.opacity = 0;
-        portDiv.style.opacity = 1;
-        sideDiv.style.marginLeft = "0px";
-        sideDiv.style.opacity = .4;
-    } else {
-        sideDiv.style.opacity = 1;
-        sideDiv.style.marginLeft = "-170px";
-        portDiv.style.opacity = 0;
-    }
+function openCurate() {
+  curates.style.marginLeft = "0px";
+  curates.style.opacity = 1;
 }
 
-function closePort() {
-    var portDiv = document.getElementById("portfolio");
-    var aboutDiv = document.getElementById("about");
-    var sideDiv = document.getElementById("sidebar");
-    aboutDiv.style.opacity = 0;
-    portDiv.style.opacity = 0;
-    sideDiv.style.opacity = 1;
-    sideDiv.style.marginLeft = "-170px";
+function closeCurate() {
+  curates.style.marginLeft = "-450px";
+  curates.style.opacity = 0;
 }
